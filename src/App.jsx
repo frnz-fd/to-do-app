812cfe79d21bfb9cbecba7abdc5859c78f1c266a
Farnaz Fadaei <farnazfadaei@outlook.com>
2023-12-11 10:48:24 +0330

    update with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -7,7 +7,26 @@ function App() {
   const [filter, setFilter] = useState('all');
   const [darkMode, setDarkMode] = useState(false);
   const [remainingItems, setRemainingItems] = useState(0);
-  const [selectedFilter, setSelectedFilter] = useState('all'); // New state for the selected filter
+  const [selectedFilter, setSelectedFilter] = useState('all');
+
+  useEffect(() => {
+    // Load todos and filter from localStorage on component mount
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    const storedFilter = localStorage.getItem('filter') || 'all';
+
+    setTodos(storedTodos);
+    setFilter(storedFilter);
+    setSelectedFilter(storedFilter);
+
+    const remainingItems = storedTodos.filter((todo) => !todo.completed).length;
+    setRemainingItems(remainingItems);
+  }, []);
+
+  useEffect(() => {
+    // Save todos and filter to localStorage whenever they change
+    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem('filter', filter);
+  }, [todos, filter]);
 
   const addTodo = () => {
     if (newTodo.trim() !== '') {
@@ -58,9 +77,9 @@ function App() {
             <h1 className='text-white text-2xl font-semibold px-6 py-12 text-left w-52'>T O D O</h1>
             <button onClick={() => setDarkMode(!darkMode)} className='w-fit h-fit py-12 px-8'>
               {darkMode ? (
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z" /></svg>
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z" /></svg>
               )}
             </button>
 
